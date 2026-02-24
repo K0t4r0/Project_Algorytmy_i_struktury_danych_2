2776a7787ac6d5929a47e8460fb45f54591197b1
Denys <denyspshen@gmail.com>
2026-02-24 12:46:52 +0100

    zadanie 1

---
+++ ZAD1 ReprezentacjaGrafow.py
@@ -0,0 +1,44 @@
+# ZADANIE 1. Reprezentacja grafow i digrafow wazonych
+# Dane wejsciowe: n - liczba wierzcholkow (numeracja od 0 do n)
+#                 m - liczba krawedzi
+#
+# Dane wyjsciowe: macierz sasiedztwa,
+#                 lista sasiadow,
+#                 dwie tablice
+n,m = map(int, input().split())
+
+matrix = [[0 for _ in range(n)] for _ in range(n)]
+lista = {i: [] for i in range(n)}
+tab1, tab2 = [], [] 
+
+for _ in range (m):
+    w1,w2,g = map(int, input().split())
+
+    matrix[w1][w2] = g
+    matrix[w2][w1] = g
+
+    lista[w1].append((w2, g))
+    lista[w2].append((w1, g))
+
+count = 0
+for i in range(n):
+    tab1.append(count)
+    for j in range(n):
+        if (matrix[i][j] != 0):
+            count += 1
+            tab2.append((j, matrix[i][j]))
+    
+print("--Macierz Sasiedztwa--")
+
+for el in matrix:
+    print(el)
+
+print("----Lista Sasiadow----")
+
+for i, v in lista.items():
+    print(f"{i}: {v}")
+
+print("-----Dwie Tablice-----")
+
+print("tab1:", tab1)
+print("tab2:", tab2)

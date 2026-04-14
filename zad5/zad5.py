1a8b5f4e7be9c81da678fa51089c1262f2f3028d
Denchik117 <deniusproficus@gmail.com>
2026-04-14 18:30:26 +0200

    zad 5 second part

--- zad5/zad5.py
+++ zad5/zad5.py
@@ -9,7 +9,7 @@ def show(points, hull_graham, hull_jarvis):
         hx, hy = zip(*(hull_graham + [hull_graham[0]]))
         plt.plot(hx, hy, 'g--', label='Graham', alpha=0.6)
 
-    if hull_jarvis: # Дениска я подготовил уже для джарвиса график нужно только передать hull_jarvis
+    if hull_jarvis:
         jx, jy = zip(*(hull_jarvis + [hull_jarvis[0]]))
         plt.plot(jx, jy, 'b:', label='Jarvis', alpha=0.6)
 
@@ -37,17 +37,50 @@ def graham_scan(points):
 
     start_point = min(points, key=lambda p: (p[1], p[0]))
     
+    points_without_start = [p for p in points if p != start_point]
+
     sorted_points = sorted(
-        points, 
+        points_without_start, 
         key=lambda p: (math.atan2(p[1] - start_point[1], p[0] - start_point[0]), dist_sq(start_point, p))
     )
 
-    hull = []
+    hull = [start_point]
     for p in sorted_points:
         while len(hull) >= 2 and get_orientation(hull[-2], hull[-1], p) != 2:
             hull.pop()
         hull.append(p)
     
+    return hull
+
+def jarvis(points):
+    n = len(points)
+    if n < 3: return points
+    
+    start_point = min(points, key=lambda p: (p[0], p[1])) 
+
+    hull = []
+    p = start_point
+    while True:
+        hull.append(p)
+
+        q = None
+
+        for r in points:
+            if r == p:
+                continue
+            if q is None:
+                q = r
+                continue
+
+            orientation = get_orientation(p, q, r)
+
+            if orientation == 2 or (orientation == 0 and dist_sq(p, r) > dist_sq(p, q)):
+                q = r
+        p = q
+
+        if p == start_point:
+            break
+
     return hull
 
 n = int(input())
@@ -61,8 +94,12 @@ else:
         points_array.append(coords)
 
     hull_g = graham_scan(points_array)
+    hull_j = jarvis(points_array)
 
     print("\n--- Graham ---")
     print(hull_g)
+
+    print("\n--- Jarvis ---")
+    print(hull_j)
     
-    show(points_array, hull_g, None)
+    show(points_array, hull_g, hull_j)
